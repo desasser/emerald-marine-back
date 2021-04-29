9a44e4f72f9f76039c2b30502983a655ad59e10a
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-04-29 14:04:30 -0700

    basic server/db file structure

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Cors config
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: whitelist,
+    credentials: true,
+    optionSuccessStatus: 200,
+    methods: 'GET, HEAD, POST, PUT'
+}
+
+
+const PORT = process.env.PORT || 8080;
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/emeraldmarine', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
