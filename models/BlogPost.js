9519afd12cfa47d1e526f62c07acbdb42076ede5
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-13 12:19:43 -0700

    added image to blog post schema

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const validateURL = url => {
+    const address = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+    return address.test(url)
+}
+
 const BlogSchema = new Schema({
     title: {
         type: String,
@@ -19,6 +24,13 @@ const BlogSchema = new Schema({
     tags: {
         type: [String],
         required: true
+    },
+    image: {
+        type: String,
+        trim: true,
+        required: true,
+        validate: [validateURL, 'Image URL must be in the following format: https://my-url-here.com'],
+        match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 'Image URL must be in the following format: https://my-url-here.com'] 
     },
     content: {
         type: String,
