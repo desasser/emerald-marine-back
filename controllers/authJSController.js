722bf599f18c18346ce91cca163cfc1ff5ebad04
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-19 15:53:30 -0700

    auth.js charge cc route

---
+++ controllers/authJSController.js
@@ -0,0 +1,99 @@
+const express = require('express');
+require('dotenv').config();
+const ApiContracts = require('authorizenet').APIContracts;
+const ApiControllers = require('authorizenet').APIControllers;
+const SDKConstants = require('authorizenet').Constants;
+
+const router = express.Router();
+
+const authClientKey = process.env.AUTHORIZE_JS_CLIENT_KEY
+const authTransactionKey = process.env.AUTHORIZE_JS_TRANSACTION_KEY
+const authUser = process.env.AUTHORIZE_JS_LOGIN_ID
+
+const chargeCreditCard = (data, cb) => {
+    const merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+    merchantAuthenticationType.setName(authUser);
+    merchantAuthenticationType.setTransactionKey(authTransactionKey);
+
+    let creditCard = new ApiContracts.CreditCardType();
+    creditCard.setCardNumber(`${data.cardNumber}`);
+    creditCard.setExpirationDate(`${data.expDate}`);
+    creditCard.setCardCode(`${data.cardCode}`);
+
+    const paymentType = new ApiContracts.PaymentType();
+    paymentType.setCreditCard(creditCard);
+
+    const billTo = new ApiContracts.CustomerAddressType();
+    billTo.setFirstName(`${data.billFirstName}`);
+    billTo.setLastName(`${data.billLastName}`);
+    billTo.setAddress(`${data.billAddress}`);
+    billTo.setCity(`${data.billCity}`);
+    billTo.setState(`${data.billState}`);
+    billTo.setZip(`${data.billZip}`);
+    billTo.setCountry(`${data.billCountry}`);
+    billTo.setEmail(`${data.email}`);
+    billTo.setPhoneNumber(`${data.phone}`);
+
+    const transactionRequestType = new ApiContracts.TransactionRequestType();
+    transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
+    transactionRequestType.setPayment(paymentType);
+    transactionRequestType.setAmount(data.amount);
+    transactionRequestType.setBillTo(billTo);
+    transactionRequestType.setCurrencyCode('USD');
+
+    const createRequest = new ApiContracts.CreateTransactionRequest();
+    createRequest.setMerchantAuthentication(merchantAuthenticationType);
+    createRequest.setTransactionRequest(transactionRequestType);
+
+    console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+
+    ctrl = new ApiControllers.CreateTransactionController(createRequest.getJSON());
+    ctrl.setEnvironment(SDKConstants.endpoint.production);
+
+    ctrl.execute(function () {
+        const apiResponse = ctrl.getResponse();
+        const response = new ApiContracts.CreateTransactionResponse(apiResponse);
+
+        if (response != null) {
+            if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
+                if (response.getTransactionResponse().getMessages() != null) {
+                    response.getTransactionResponse().getTransId();
+                    response.getTransactionResponse().getResponseCode();
+                    response.getTransactionResponse().getMessages().getMessage()[0].getCode();
+                    response.getTransactionResponse().getMessages().getMessage()[0].getDescription();
+                }
+                else {
+                    if (response.getTransactionResponse().getErrors() != null) {
+                        response.getTransactionResponse().getErrors().getError()[0].getErrorCode();
+                        response.getTransactionResponse().getErrors().getError()[0].getErrorText();
+                    }
+                }
+            }
+            else {
+                console.log('Failed Transaction. ');
+                if (response.getTransactionResponse() != null && response.getTransactionResponse().getErrors() != null) {
+                    response.getTransactionResponse().getErrors().getError()[0].getErrorCode();
+                    response.getTransactionResponse().getErrors().getError()[0].getErrorText();
+                }
+                else {
+                    response.getMessages().getMessage()[0].getCode();
+                    response.getMessages().getMessage()[0].getText();
+                }
+            }
+        }
+        cb(response);
+    });
+}
+
+router.post('/authjs/charge', (req, res) => {
+    chargeCreditCard(req.body, function(response) {
+        res.json(response)  
+    });
+});
+
+
+
+
+// console.log(ApiContracts)
+
+module.exports = router;
--- controllers/upsController.js
+++ controllers/upsController.js
@@ -8,15 +8,12 @@ const upsKey = process.env.UPS_API_ACCESS_KEY
 const upsUser = process.env.UPS_USERNAME
 const upsPassword = process.env.UPS_PASSWORD
 
-const authClientKey = process.env.AUTHORIZE_JS_CLIENT_KEY
-const authTransactionKey = process.env.AUTHORIZE_JS_TRANSACTION_KEY
-const authUser = process.env.AUTHORIZE_JS_LOGIN_ID
 
 
 router.get('/ups', (req, res) => {
-    console.log(authClientKey);
-    console.log(authUser);
-    console.log(authTransactionKey);
+    console.log(upsKey);
+    console.log(upsUser);
+    console.log(upsPassword);
 });
 
 module.exports = router;
