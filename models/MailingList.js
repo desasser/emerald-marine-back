aaa9dffef359821afea972cc370ed253b1e31575
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-24 12:02:53 -0700

    created mailing list models

---
+++ models/MailingList.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const validateEmail = email => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+}
+
+const MailingListSchema = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: 'Email address is required.',
+        validate: [validateEmail, 'Email must be in the following format: abc@def.com'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email must be in the following format: abc@def.com']
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: 'Name is required.'
+    }
+});
+
+const MailingList = mongoose.model('MailingList', MailingListSchema)
+module.exports = MailingList;
