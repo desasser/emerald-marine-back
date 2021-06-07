3dd7c148e997909042f8764cc19b4a032c1a16d1
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-07 13:42:02 -0700

    added all schemes to schema composer

--- graphql/SchemaComposer.js
+++ graphql/SchemaComposer.js
@@ -3,11 +3,29 @@ const {SchemaComposer} = require('graphql-compose');
 const schemaComposer = new SchemaComposer();
 
 const {BlogPostQuery, BlogPostMutation} = require('./BlogPostScheme');
+const { PressReleaseMutation, PressReleaseQuery } = require('./PressReleaseScheme');
+const { ProductQuery, ProductMutation } = require('./ProductScheme');
+const { ProductTestQuery, ProductTestMutation } = require('./ProductTestScheme');
+const {NewsArticleQuery, NewsArticleMutation} = require('./NewsArticleScheme');
+const {MailingListQuery, MailingListMutation} = require('./MailingListScheme');
+const {UserQuery, UserMutation} = require('./UserScheme');
 
 schemaComposer.Query.addFields({
-    ...BlogPostQuery
+    ...BlogPostQuery,
+    ...MailingListQuery,
+    ...NewsArticleQuery,
+    ...PressReleaseQuery,
+    ...ProductQuery,
+    ...ProductTestQuery,
+    ...UserQuery
 });
 
 schemaComposer.Mutation.addFields({
-    ...BlogPostMutation
+    ...BlogPostMutation,
+    ...MailingListMutation,
+    ...NewsArticleMutation,
+    ...PressReleaseMutation,
+    ...ProductMutation,
+    ...ProductTestMutation,
+    ...UserMutation
 });
