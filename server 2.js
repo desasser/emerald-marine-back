965a42841dcc0a4bcf8f5aa0e4c926fbec00a342
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-22 13:43:29 -0700

    changed blog and product seed files

---
+++ server 2.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: whitelist,
+    credentials: true,
+    optionSuccessStatus: 200,
+    methods: 'GET, HEAD, POST, PUT'
+}
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/emeraldmarine', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const userRoutes = require('./controllers/userController');
+const productRoutes = require('./controllers/productController');
+const blogRoutes = require('./controllers/blogPostController');
+const newsRoutes = require('./controllers/newsController');
+const mailingRoutes = require('./controllers/mailingListController');
+const testRoutes = require('./controllers/productTestListController');
+const pressRoutes = require('./controllers/pressController');
+const authjsRoutes = require('./controllers/authjsController');
+const shippoRoutes = require('./controllers/shippoController');
+const imageRoutes = require('./controllers/imageController');
+
+app.use(blogRoutes);
+app.use(productRoutes);
+app.use(userRoutes);
+app.use(newsRoutes);
+app.use(pressRoutes);
+app.use(mailingRoutes);
+app.use(testRoutes);
+app.use(authjsRoutes);
+app.use(shippoRoutes);
+app.use(imageRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
