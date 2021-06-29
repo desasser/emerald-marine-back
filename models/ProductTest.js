5a5b26cdbeeeb80cbc724ada0afd9ac672db146d
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-29 10:42:32 -0700

    added keys to mailing and test list schema

--- models/ProductTest.js
+++ models/ProductTest.js
@@ -15,6 +15,21 @@ const ProductTestSchema = new Schema({
         required: 'Email address is required.',
         validate: [validateEmail, 'Email must be in the following format: abc@def.com'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email must be in the following format: abc@def.com']
+    },
+    first: {
+        type: String,
+        trim: true,
+        required: 'First name is required.'
+    },
+    last: {
+        type: String,
+        trim: true,
+        required: 'Last name is required.'
+    },
+    company: {
+        type: String,
+        trim: true,
+        required: 'Company name is required.'
     }
 });
 
