790090ff7c0024744d4bd18839d09ee3bcaf0c4f
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-08-29 13:56:18 -0700

    console logs

--- controllers/captchaController.js
+++ controllers/captchaController.js
@@ -6,8 +6,10 @@ require('dotenv').config();
 const router = express.Router();
 
 router.post('/hooman', (req, res) => {
-    const isHooman = response => {
-        axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET}&response=${response}`).then(result => {
+    const isHooman = token => {
+        console.log(`Here's the token: ${token}`)
+        console.log(`Here's the whole thing: ${req.body}`, null, 2);
+        axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET}&response=${token}`).then(result => {
             result.data.success ? res.json(result.data) : res.send('Response not valid.')
         }).catch(err => {
             err ? res.status(403).send(`Error: ${err}`) : res.status(200).send('Verified.')
